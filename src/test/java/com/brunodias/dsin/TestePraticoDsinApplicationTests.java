f795160581c2aa52d1771ff40e276af288121431
brunodias77 <brunohdias95@gmail.com>
2024-10-17 08:09:49 -0300

    first commit

---
+++ src/test/java/com/brunodias/dsin/TestePraticoDsinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunodias.dsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoDsinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
